f2a9f0d4a7e0c86606bcfbdc919b565661459324
Oktawski <oktawian.perlowski@gmail.com>
2022-04-16 22:18:15 +0200

    UI Update: Added states to Login component

--- PasswordManager/ClientApp/src/components/Login.jsx
+++ PasswordManager/ClientApp/src/components/Login.jsx
@@ -12,6 +12,11 @@ import {
 export class LoginPage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            username: "",
+            password: ""
+        };
     }
 
     render() {
@@ -21,14 +26,14 @@ export class LoginPage extends React.Component {
                 <form>
                     <FormGroup sx={{ mx: 'auto', mt: 8, width: '50%' }}>
                         <FormControl>
-                            <TextField id='username-input' label='Username' variant='outlined'/>
+                            <TextField id='username-input' label='Username' variant='outlined' value={this.state.username} onChange={e => this.setState({ username: e.target.value})}/>
                         </FormControl>
 
                         <FormControl sx={{ mt: 4 }}>
-                            <TextField id='password-input' type='password' label='Password' variant='outlined'/>
+                            <TextField id='password-input' type='password' label='Password' variant='outlined' value={this.state.password} onChange={e => this.setState({ password: e.target.value })}/>
                         </FormControl>
 
-                        <Button sx={{ mt: 3, mx: 'auto', width: '50%' }} variant='contained' type='submit'>Login</Button>
+                        <Button sx={{ mt: 3, mx: 'auto', width: '50%' }} variant='contained' onClick={() => alert("Implement service layer you fool")}>Login</Button>
 
                     </FormGroup>
                 </form>
