82403563fc8977f1814229e53812a5e09dda5c55
Oktawski <oktawian.perlowski@gmail.com>
2022-07-01 14:21:23 +0200

    Added tests to PasswordService

--- PasswordManager.Tests/PasswordServiceTest.cs
+++ PasswordManager.Tests/PasswordServiceTest.cs
@@ -20,12 +20,22 @@ public class PasswordServiceTest : IDisposable
         db = GetInMemoryDbContext();
 
         _service = new PasswordService(db, claimsPrincipal);
+
+        PopulateDb();
     }
     
     public void Dispose()
     {
+        var users = db.Users;
+        db.Users.RemoveRange(users);
+        db.SaveChanges();
+
+        var passwords = db.Passwords;
+        db.Passwords.RemoveRange(passwords);
+        db.SaveChanges();
     }
 
+    
     private static ClaimsPrincipal GetClaimsPrincipal()
     {
         var claims = new List<Claim>()
@@ -45,7 +55,28 @@ public class PasswordServiceTest : IDisposable
          
         return new ApplicationDbContext(optionsBuilder.Options);
     }
+
+    private void PopulateDb()
+    {
+        var user = new ApplicationUser()
+        {
+            UserName = "test", 
+            Id = "applicationUser", 
+            PasswordHash = "hashedPassword" 
+        };
+
+        db.Users.Add(user);
+        db.SaveChanges();
+    }
     
+
+    [Fact]
+    public void Get_Prepopulated_Users_Set()
+    {
+        var users = db.Users.ToList();
+
+        Assert.NotEmpty(users);
+    }
 
     [Fact]
     public async void Add_Password()
@@ -60,10 +91,27 @@ public class PasswordServiceTest : IDisposable
     }
 
     [Fact]
-    public async void Get_All()
+    public async void Get_All_Returns_Empty_Collection()
     {
         var passwords = await _service.GetAll();
 
         Assert.Empty(passwords);
+    }
+
+    [Fact]
+    public async void Add_Get_Password()
+    {
+        var passwordApplication = "application";
+        var passwordValue = "value";
+
+        var isSuccess = await _service.Add(new AddPasswordRequest(passwordApplication, passwordValue));
+
+        Assert.True(isSuccess);
+
+        var password = db.Passwords.ToList().FirstOrDefault();
+
+        Assert.NotNull(password);
+        Assert.Equal(passwordApplication, password!.Application);
+        Assert.Equal(passwordValue, password!.Value);
     }
 }
